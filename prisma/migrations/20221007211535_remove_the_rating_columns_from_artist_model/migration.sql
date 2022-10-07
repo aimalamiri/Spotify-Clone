324cf03167b5e9808ca0faa73577046096f16257
Aimal Amiri <meaimal@hotmail.com>
2022-10-08 01:49:14 +0430

    Add songsData seed

---
+++ prisma/migrations/20221007211535_remove_the_rating_columns_from_artist_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `rating` on the `Artist` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Artist" DROP COLUMN "rating";
